e64c5a52377cfc2cda10a8ec61f45e2af026c3e6
John Muinde <jhnmuinde@gmail.com>
2022-06-23 17:32:43 +0300

    My first one

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,15 @@
-const button5 = document.querySelector("#plusFive")
+window.onload = () =>{
+
+
 const indicator = document.querySelector("#number-indicator")
 const disp = document.querySelector("#display-heading")
+const buttons = [...document.querySelectorAll("button")]
 
-button5.addEventListener('click', () => {
-  disp.textContent = "added 5"
-  indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
+
+buttons.forEach(element =>{
+  element.addEventListener('click',()=>{
+    disp.textContent = `added ${(buttons.indexOf(element) + 1) * 5}`;
+    indicator.textContent = (parseInt(indicator.textContent) + ((buttons.indexOf(element) + 1) * 5)).toString();
 })
+});
+}
